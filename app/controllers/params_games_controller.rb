b56b236dd55883f377f45df26187b0813f88ee10
GB-actualize <gbactualize@gmail.com>
2016-08-30 20:03:22 -0500

    upto 'begins with a' game

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -1,6 +1,11 @@
 class ParamsGamesController < ApplicationController
   def query
-    @name = params[:name].upcase
+    @name = params[:name]
+    @nombre = params[:name].upcase
+    @sentence = "#{@name} doesn't begin with an 'a'."
+    if @name.first == 'a'
+        @sentence = "#{@name} begins with an 'a'."
+    end
   end
 end
 
